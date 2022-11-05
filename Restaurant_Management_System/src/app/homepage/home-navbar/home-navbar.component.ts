e8e439897e1383484120d7bec2e3b218e469117e
Binuka Silva <wabgsilva@gmail.com>
2022-11-05 18:55:28 +0530

    NavBar Done

---
+++ Restaurant_Management_System/src/app/homepage/home-navbar/home-navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-home-navbar',
+  templateUrl: './home-navbar.component.html',
+  styleUrls: ['./home-navbar.component.scss']
+})
+
+export class HomeNavbarComponent{}
